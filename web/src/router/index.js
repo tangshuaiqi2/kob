92528395c792f617f11ac4163048f26955bbc4d7
tangshuaiqi <tangshuaiqi@qq.com>
2022-12-12 17:30:39 +0800

    实现了导航栏+对战页面的地图

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView'
+import UserBotIndexView from '../views/user/bots/UserBotIndexView'
+import NotFound from '../views/error/NotFound'
+
 
 const routes = [
-
+  {
+    path: "/",
+    name: "home_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
+  }
   
 ]
 
